4fb82dc9461a617f2988ab187ba0aa76e7562080
lucasvida <lucasvida@gmail.com>
2024-11-12 17:38:41 -0300

    Adicionado Local Storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const createTask = document.querySelector("#task");
+const createDescription = document.querySelector("#description");
 const tarefas = document.querySelector("#list ul");
 const form = document.querySelector("form");
 let checkBox;
@@ -6,25 +7,52 @@ let excluir;
 
 let listOfTask = [];
 
+const options = {
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+}
+
+class Task {
+  constructor(task, description, date, done) {
+    this.task = task;
+    this.description = description;
+    this.date = date;
+    this.done = done;
+  }
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  listOfTask.push(createTask.value);
+  const newTask = new Task(createTask.value, createDescription.value, new Date(), false);
+  listOfTask.push(newTask);
+  localStorage.setItem("list", JSON.stringify(listOfTask));
   createTask.value = "";
+  createDescription.value = "";
   criaLista();
 });
 
 function criaLista() {
   tarefas.innerHTML = "";
-  for (const itens of listOfTask) {
-    const index = listOfTask.indexOf(itens);
+
+  const list = JSON.parse(localStorage.getItem("list"));
+
+  
+  list.forEach((itens, index) => {
+    //Converte itens.date em data.
+    itens.date = new Date(itens.date);
     const li = document.createElement("li");
     li.innerHTML = `
-           <input type="checkbox" data-id="${index}" id="check-${index}">
-<label for="check-${index}">${itens}</label>
-<a href="#" id="${index}"><i class="bi bi-trash3-fill"></i></a>
-        `;
+      <input type="checkbox" data-id="${index}" id="check-${index}" ${itens.done ? 'checked' : ''}>
+      <label for="check-${index}" class="${itens.done ? 'done' : 'none'}">
+  ${itens.task} | ${itens.date.toLocaleDateString('pt-BR',options)}
+</label>
+      <p>${itens.description}</p>
+      <a href="#" id="${index}"><i class="bi bi-trash3-fill"></i></a>
+    `;
     tarefas.append(li);
-  }
+  });
+
   excluir = document.querySelectorAll("a");
   checkBox = document.querySelectorAll("input[type='checkbox']");
   removeItem();
@@ -35,7 +63,8 @@ function removeItem() {
   for (let link of excluir) {
     link.addEventListener("click", () => {
       listOfTask.splice(link.id, 1);
-      criaLista();
+      localStorage.setItem("list", JSON.stringify(listOfTask));
+      criaLista();  // Atualizar a lista após excluir
     });
   }
 }
@@ -43,33 +72,35 @@ function removeItem() {
 function checkItem() {
   for (let check of checkBox) {
     check.addEventListener("change", () => {
-      const getTask = document.querySelector(
-        `label[for="check-${check.dataset.id}"]`
-      );
+      const index = check.dataset.id;
+      const getTask = document.querySelector(`label[for="check-${index}"]`);
+
       if (check.checked) {
-        getTask.style.textDecoration = "line-through";
-        getTask.style.color = "red";
+        listOfTask[index].done = true;
+        getTask.classList.add('done');
+        getTask.classList.remove('none');
       } else {
-        getTask.style.textDecoration = "";
-        getTask.style.color = "";
+        listOfTask[index].done = false;
+        getTask.classList.remove('done');
+        getTask.classList.add('none');
+        
       }
+
+      localStorage.setItem("list", JSON.stringify(listOfTask));  // Atualiza o estado no localStorage
     });
   }
 }
 
-
 /* Menu Off-Canvas */
-
 const config = document.querySelector(".btn-config");
 const menuOfCanvas = document.querySelector(".menu-offcanvas");
 
-config.addEventListener("click", ()=>{
-    menuOfCanvas.style.marginRight = 0;
-})
-
+config.addEventListener("click", () => {
+  menuOfCanvas.style.marginRight = 0;
+});
 
 const close = document.querySelector(".close");
 
-close.addEventListener("click", ()=>{
+close.addEventListener("click", () => {
   menuOfCanvas.style.marginRight = "-500px";
-})
+});
